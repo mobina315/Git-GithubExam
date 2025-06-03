b7b3f9d09998c03cfc9f1fda0fc77c932032a85d
mobina Ataei <mobina.atae@gmail.com>
2025-06-03 11:05:30 +0330

    taghir file

---
+++ DisplaySimpleFigureWithJava.java
@@ -0,0 +1,23 @@
+public class DisplaySimpleFigureWithJava {
+    public static void main(String[] args) {
+        ninety();
+        System.out.println();
+        ninety();
+        ninety();
+    }
+
+    public static void ninety() {
+        System.out.println("* | | | | | *");
+    }
+}
+
+public static void stars(){
+
+    System.out.println("  ***  ");
+    System.out.println("  ******  ");
+    System.out.println("  *******  ");
+
+
+}
+
+
